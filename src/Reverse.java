83e10845f9b42be8d767adea351b82a2078e2392
testingy12345 <tekadevipin@gmail.com>
2017-12-24 16:12:38 +0530

    This is Predefined String

---
+++ src/Reverse.java
@@ -0,0 +1,22 @@
+import java.nio.charset.MalformedInputException;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Reverse {
+	static int i ,c=0 ,res ;
+	static void Stringreverse(String s) {
+		char ch[]=new char[s.length()];
+		for(i=0 ;i<s.length();i++) {
+			for(i=s.length()-1 ;i>=0 ; i--) {
+				System.out.println(ch[i]);
+				public static void main(String args[]) {
+					System.out.println("Original String is: ");
+					System.out.println("My name is Vipin");
+					Reverse.Stringreverse("My name is Vipin");
+					
+				}
+			}
+		}
+	}
+
+}
